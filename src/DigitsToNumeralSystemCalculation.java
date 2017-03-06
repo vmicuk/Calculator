fa1b17e8e7cd04a8845aa30ec8c3e927da07135e
evelina <e.daugirdaite@google.com>
2017-03-06 18:31:35 +0200

    Added new class DigitsToNumeralSystemCalculation, int to binary, octal, hex and back.

---
+++ src/DigitsToNumeralSystemCalculation.java
@@ -0,0 +1,22 @@
+/**
+ * Created by student on 17.3.6.
+ */
+public class DigitsToNumeralSystemCalculation {
+
+    private int number;
+
+    public DigitsToNumeralSystemCalculation() {
+    }
+
+    public DigitsToNumeralSystemCalculation(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
