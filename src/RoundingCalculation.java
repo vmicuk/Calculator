29e36c20f23e82d92048d0ce61827463875dc216
vmicuk <vitalij.micuk@gmail.com>
2017-03-06 18:44:27 +0200

    RoundingCalculator.java , delete getter and setter and constructor

--- src/RoundingCalculation.java
+++ src/RoundingCalculation.java
@@ -8,18 +8,6 @@ public class RoundingCalculation {
     public RoundingCalculation() {
     }
 
-    public RoundingCalculation(double number) {
-        this.number = number;
-    }
-
-    public double getNumber() {
-        return number;
-    }
-
-    public void setNumber(double number) {
-        this.number = number;
-    }
-
     public double round (double number) {
         return (Math.round(number*100.0)/100.0);
     }
