ad3079261f6351634ea091d8b26083397da83d48
vmicuk <vitalij.micuk@gmail.com>
2017-03-06 17:30:25 +0200

    Trigonometry add, create 4 methods sinA, cosA, tanA, ctanA.

---
+++ src/TrigonometryCalculator.java
@@ -0,0 +1,20 @@
+/**
+ * Created by stsudent on 17.3.6.
+ */
+public class TrigonometryCalculator {
+
+    public double sinA(double a){
+        return Math.sin(a);
+    }
+
+    public double cosA(double a){
+        return Math.cos(a);
+    }
+
+    public double tgA(double a) { return Math.tan(a);    }
+
+    public double ctgA(double a) {
+        return 1.0/Math.tan(a);
+    }
+
+}
